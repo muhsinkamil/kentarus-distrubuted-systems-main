2b8d26b4aac1ac0dcd8c1e7980f2748cf3e77105
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-18 22:16:54 +0530

    Initialise java project

---
+++ src/main/java/com/kentarus/distributed_systems/DistributedSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.kentarus.distributed_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSystemsApplication.class, args);
+	}
+
+}
