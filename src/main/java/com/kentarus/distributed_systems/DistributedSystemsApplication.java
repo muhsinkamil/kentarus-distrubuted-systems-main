103ec6000400da655ee7ea57cb3dfd44cabd5f25
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-19 01:37:03 +0530

    Add rest template bean

--- src/main/java/com/kentarus/distributed_systems/DistributedSystemsApplication.java
+++ src/main/java/com/kentarus/distributed_systems/DistributedSystemsApplication.java
@@ -2,12 +2,19 @@ package com.kentarus.distributed_systems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DistributedSystemsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedSystemsApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/kentarus/distributed_systems/controllers/WordController.java
+++ src/main/java/com/kentarus/distributed_systems/controllers/WordController.java
@@ -4,12 +4,16 @@ import com.kentarus.distributed_systems.services.WordService;
 import com.kentarus.distributed_systems.structures.DeleteResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/words")
@@ -17,22 +21,36 @@ public class WordController {
 
     @Autowired
     WordService wordService;
+
+    @Autowired
+    RestTemplate restTemplate;
+
     String OKStatus = "OK";
     String NOKStatus = "NOK";
 
     @GetMapping()
-    public String[] getWords() {
-        String[] words = new String[5];
-        return words;
+    public String getWords() {
+        // String[] words = new String[5];
+
+        String result = restTemplate.getForObject("http://localhost:9000/words", String.class);
+        return result;
     }
 
     @DeleteMapping()
-    public ResponseEntity<DeleteResponse> deleteWords() {
-        return ResponseEntity.ok().body(new DeleteResponse(OKStatus));
+    public ResponseEntity<String> deleteWords() {
+        ResponseEntity<String> result = restTemplate.exchange("http://localhost:9000/words", HttpMethod.DELETE,
+                new HttpEntity<String>("L"), String.class);
+        return result;
+        // return ResponseEntity.ok().body(new DeleteResponse(OKStatus));
     }
 
-    @DeleteMapping(value = "{word}")
-    public ResponseEntity<DeleteResponse> deleteWord(@PathVariable String word) {
-        return ResponseEntity.ok().body(new DeleteResponse(word));
+    @DeleteMapping(value = "{wordValue}")
+    public ResponseEntity<String> deleteWord(@PathVariable String word) {
+        // return ResponseEntity.ok().body(new DeleteResponse(word));
+        // HttpHeaders headers = new HttpHeaders();
+
+        ResponseEntity<String> result = restTemplate.exchange("http://localhost:9000/words", HttpMethod.DELETE,
+                new HttpEntity<String>("L"), String.class);
+        return result;
     }
 }
