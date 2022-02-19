927cd8c8f8b9332c40feeee936909d596a4b300b
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-19 17:34:57 +0530

    Add GET and DEL all words

--- src/main/java/com/kentarus/distributed_systems/DistributedSystemsApplication.java
+++ src/main/java/com/kentarus/distributed_systems/DistributedSystemsApplication.java
@@ -2,8 +2,6 @@ package com.kentarus.distributed_systems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DistributedSystemsApplication {
@@ -11,10 +9,4 @@ public class DistributedSystemsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedSystemsApplication.class, args);
 	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-
 }
---
+++ src/main/java/com/kentarus/distributed_systems/constants/InstancesUrl.java
@@ -0,0 +1,22 @@
+package com.kentarus.distributed_systems.constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InstancesUrl {
+    public static final String firstInstance = "http://localhost:9000";
+    public static final String secondInstance = "http://localhost:9000";
+    public static final String thirdInstance = "http://localhost:9000";
+    public static final String fourthInstance = "http://localhost:9000";
+    public static final String fifthInstance = "http://localhost:9000";
+
+    public static final Map<String, String> instances = new HashMap<String, String>() {
+        {
+            put("firstInstance", firstInstance);
+            put("secondInstance", secondInstance);
+            put("thirdInstance", thirdInstance);
+            put("fourthInstance", fourthInstance);
+            put("fifthInstance", fifthInstance);
+        }
+    };
+}
